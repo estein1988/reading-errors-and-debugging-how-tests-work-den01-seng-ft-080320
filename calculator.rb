5fea7b98cf5fc5a4e700867c4e4d08c0bc5cef67
Elliott Stein <estein1988@gmail.com>
2020-06-30 15:53:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,3 +5,9 @@ first_number = 42
 second_number = 84
 
 sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
